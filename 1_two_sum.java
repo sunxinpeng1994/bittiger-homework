3201b0593e220dde303a2f80fc6d443a1e3f1cea
sunxinpeng1994 <sunxinpeng1994@gmail.com>
2016-12-18 07:55:14 -0600

    Update 1_two_sum.java

--- 1_two_sum.java
+++ 1_two_sum.java
@@ -1,40 +1,4 @@
-Last login: Sun Dec 18 07:43:42 on ttys000
-XinpengdeMacBook-Pro:~ xinpengsun$ ls
-Applications	Documents	Library		Music		Public
-Desktop		Downloads	Movies		Pictures	eclipse
-XinpengdeMacBook-Pro:~ xinpengsun$ Desktop
--bash: Desktop: command not found
-XinpengdeMacBook-Pro:~ xinpengsun$ cd /Users/apple/Desktop/
--bash: cd: /Users/apple/Desktop/: No such file or directory
-XinpengdeMacBook-Pro:~ xinpengsun$ cd Desktop
-XinpengdeMacBook-Pro:Desktop xinpengsun$ bittiger-homework
--bash: bittiger-homework: command not found
-XinpengdeMacBook-Pro:Desktop xinpengsun$ ls
-CV_Xinpeng SUN.doc
-Game Design
-Intro to Database System
-Java数据结构和算法.（第二版）.pdf
-Matlab 2012b下载地址及破解安装教程.pdf
-SAMPLE (Cleaned) copy.pdf
-[java编程思想中文(第4版)]-2007.pdf
-bittiger-homework
-cv_ppt by zjm
-屏幕快照 2016-12-13 21.48.34.png
-XinpengdeMacBook-Pro:Desktop xinpengsun$ bittiger-homework
--bash: bittiger-homework: command not found
-XinpengdeMacBook-Pro:Desktop xinpengsun$ ls
-CV_Xinpeng SUN.doc
-Game Design
-Intro to Database System
-Java数据结构和算法.（第二版）.pdf
-Matlab 2012b下载地址及破解安装教程.pdf
-SAMPLE (Cleaned) copy.pdf
-[java编程思想中文(第4版)]-2007.pdf
-bittiger-homework
-cv_ppt by zjm
-屏幕快照 2016-12-13 21.48.34.png
-XinpengdeMacBook-Pro:Desktop xinpengsun$ cd bittiger-homework
-XinpengdeMacBook-Pro:bittiger-homework xinpengsun$ vim 1_two_sum.java
+
 
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
@@ -54,9 +18,5 @@ public class Solution {
         return ans;
     }
 }
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
-~                                                                               
+                                                                         
 
