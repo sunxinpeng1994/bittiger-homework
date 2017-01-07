8111527953453525d958881073568d7dbc95971d
sunxinpeng1994 <sunxinpeng1994@gmail.com>
2017-01-07 14:27:34 -0600

    Add files via upload

---
+++ 145_binary_postorder_traversal.java
@@ -0,0 +1,31 @@
+145_binary_postorder_traversal.java
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        if(root == null){
+            return res;
+        }
+        while(!stack.isEmpty() || root != null){
+            if(root != null){
+                stack.push(root);
+                res.add(root.val);
+                root = root.right;
+            }
+            else{
+                root = stack.pop().left;
+            }
+        }
+        Collections.reverse(res);
+        return res;
+    }
+}
